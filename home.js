b2ac959a67bb013f8438dfea62f7979a1ab4f115
수민 <5h6sm.github@gmail.com>
2022-09-23 07:35:14 +0900

    first commit

---
+++ home.js
@@ -0,0 +1,13 @@
+  $('.content').mousewheel(function(e){
+    if( e.deltaY < 0 ){
+      $('.hero, .content').addClass('scrolled');
+      return false;
+    }
+  });
+  $(window).mousewheel(function(e){
+    if( $('.hero.scrolled').length ){
+      if( $(window).scrollTop() == 0 && e.deltaY > 0 ){
+        $('.hero, .content').removeClass('scrolled');
+      }
+    }
+  });
